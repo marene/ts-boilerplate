a2c1be154b375ed45d2ff2d210212c70a0a0c4d8
Rosa <rosa.berned@wefox.com>
2021-10-25 16:43:53 +0200

    add new fucntiones done

--- src/level_03.ts
+++ src/level_03.ts
@@ -1,3 +1,7 @@
+import { Student } from './level_03';
+
+
+
 export class Student {
     constructor(private readonly _age: number, private readonly _name: string) {}
 
@@ -18,11 +22,31 @@ export class Classroom {
     }
 
     addStudent(student: Student): void {
+        const arrStudents = this._students;
+        arrStudents.push(student);
+        return;
     }
 
+    
+
     findYoungestStudent(): Student|void {
-        return new Student(0, 'John Doe')
-    }
+        //const youngest = this._students.Math.min(Student, 0);
+        const students = this._students;
+        const youngest = students.find( student => students.length.Math.min(students))
+
+        // let minimunAge = this._students[0];
+       
+        // for (let i= 0; i < this._students.length; i++) {
+        //     if(this._students[i] < minimunAge){
+        //     minimunAge = this._students[i];
+        //       } else if (this._students[i] > minimunAge){
+        //           this._students.Math.max(this.students [i];
+        //       }
+        //  return;
+        //return new Student(0, 'John Doe')
+       return youngest; 
+    
+        }
 
     findOldestStudent(): Student|void {
         return new Student(99, 'John Doe')
@@ -34,4 +58,6 @@ export class Classroom {
 
     removeStudentsByName(name: string): void {
     }
-}
+        
+        
+    } 
