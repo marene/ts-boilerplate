f9795674cb4ffa046443049ddf8feffb329565e7
Rosa <rosa.berned@wefox.com>
2021-11-08 17:16:11 +0100

    add first tests round

--- src/level_01.ts
+++ src/level_01.ts
@@ -2,7 +2,7 @@
 * THis interface describes a car.
 * power is the horsepower of the cars, color is the color of the car (duh)
 */
-export interface Car {
+interface Car {
     power: number;
     color: string;
 };
@@ -15,7 +15,21 @@ export interface Car {
 *
 */
 export function countCarsByColorsCaseInsensitive(cars: Car[]): { [color: string]: number } {
-    return {};
+    const countCarsByColors: { [color: string]: number } = {}
+
+    for( const car of cars ) {
+        const lowerCaseColor =  car.color.toLowerCase();
+        if(countCarsByColors[lowerCaseColor] == undefined){
+            countCarsByColors[lowerCaseColor] = 0
+        }
+
+         countCarsByColors [lowerCaseColor] += 1
+
+
+    }
+
+
+    return countCarsByColors;
 }
 
 /*
@@ -24,41 +38,113 @@ export function countCarsByColorsCaseInsensitive(cars: Car[]): { [color: string]
 * returns: { blue: 2, red: 1 }
 */
 export function countCarsByColors(cars: Car[]): { [color: string]: number } {
-    return {};
+    const carCountColors: { [color: string]: number } = {}
+        
+    for (const car of cars) {
+        if(carCountColors[car.color] == undefined){
+            carCountColors[car.color] = 0
+        }
+        carCountColors[car.color] += 1
+
+    }
+    console.log(carCountColors);
+    return carCountColors;
 }
+
+// const empyObj = {};
+// const key = 'blue';
+
+// emptyObj[key] = 0;
+
+// // { blue: 0 }
+
+// for (const key of iterator) {
+//     console.log(key);
+//   }
 
 /*
 * Returns the first car with biggest horsepower found in the array passed as parameter
 */
 export function findCarWithBiggestPower(cars: Car[]): Car | void {
-    return undefined;
+    const carBiggestPower = cars. sort((car1, car2) => car1.power - car2.power)
+    const BiggestOne = carBiggestPower[carBiggestPower.length -1]
+    console.log(BiggestOne);
+    
+        return BiggestOne;
 }
 
 /*
 * Returns the first car with smallest horsepower found in the array passed as parameter
 */
 export function findCarWithSmallestPower(cars: Car[]): Car | void {
-    return undefined;
+    const carSmallPower = cars. sort((car1, car2) => car1.power - car2.power)
+    const first = carSmallPower[0]
+  
+      return  first;
+
 }
 
-/*
-* Returns a new array containing all the cars passed as parameter, sorted by ascending horsepower
-*/
-export function sortCarsByHorsePower(cars: Car[]): Car[] {
-    return [];
-}
+    //     let carSmallPower;
+
+//     for (const car of cars) {
+//       const carPower = car.power;
+//       if (carPower <= 10 && carSmallPower === undefined) {
+//         console.log(carSmallPower);
+//         carSmallPower = car;
+       
+//       }
+//     } 
+    
+//     return undefined;
+ 
+// }
+
+// /*
+// * Returns a new array containing all the cars passed as parameter, sorted by ascending horsepower
+// */
+ export function sortCarsByHorsePower(cars: Car[]): Car[] { 
+     const sortedCars= cars.sort((car1, car2) => car1.power - car2.power)
+    
+        return sortedCars; 
+ }
 
 /*
 * Adds 10 hp to the power of each cars in the array passed as parameter
 */
-export function overPowerAllCars(cars: Car[]): Car[] {
-    return [];
+export function overPowerAllCars(cars: Car[]): Car[]{
+    let newCars =[];
+    
+    for (let car of cars) {
+        newCars.push({ color: car.color, power: car.power + 10 })  
+    }
+
+    return newCars; 
 }
+
+//with map() 
+
+/* export function overPowerAllCars(cars: Car[]): Car[]{
+     return cars.map(car => ({
+        color: car.color,
+        power: car.power +10
+    }));
+
+}
+
+
+    */
+
+
 
 /*
 * returns a car with the given color and power.
 * if the power isn't passed as a parameter, creates a car with a default power of 10 hp
 */
-export function createCar(color: string, power?: number): Car {
-    return {} as unknown as Car;
+export function createCar(color: string, power: number = 10): Car {
+    const car = {
+        color,
+        power
+    } 
+
+    return car;
 }
--- tests/level_01.test.ts
+++ tests/level_01.test.ts
@@ -1,95 +1,332 @@
-import * as cars from '../src/level_01';
+import * as cars from '../src/level_01'; // { createCar: () => {}, sortCarsByHorsePower: () => {} }
 
 describe('level_01', () => {
     describe('createCar', () => {
         it('should return a car with the right color and power', () => {
+            const car = cars.createCar('green', 42);
+
+            expect(car).toEqual({
+                color: 'green',
+                power: 42,
+            });
          });
 
         it('should return a car with the right color and default power', () => {
+            const car = cars.createCar('green', 10);
+
+            expect(car).toEqual({
+                color: 'green', 
+                power: 10
+            });            
         });
     });
 
     describe('overPowerAllCars', () => {
         it('should add 10 to every cars power', () => {
+            const overPowerCars = cars.overPowerAllCars([
+                cars.createCar ('blue', 2),
+                cars.createCar ('red', 10),
+                cars.createCar ('yellow', 30)  
+            ]);
+
+            expect(overPowerCars).toEqual([
+                { color: 'blue', power: 12 },
+                { color: 'red', power: 20 },
+                { color: 'yellow', power: 40 }, 
+            ]);
         });
 
         it('should do nothing if the array of cars is empty', () => {
+            const overPowerCars = cars.overPowerAllCars([]);
+
+            expect(overPowerCars).toEqual([]);
         });
     });
 
     describe('sortCarsByHorsePower', () => {
         it('should do nothing if the array is already sorted', () => {
+            const sortCarsPower = cars.sortCarsByHorsePower([
+                cars.createCar ('blue', 2),
+                cars.createCar ('red', 10),
+                cars.createCar ('yellow', 30)
+            ]);
+                
+                expect(sortCarsPower).toEqual([
+                    { color: 'blue', power: 2 },
+                    { color: 'red', power: 10 },
+                    { color: 'yellow', power: 30 }, 
+                ]);     
         });
 
         it('should sort the array', () => {
+            const sortCarsPower = cars.sortCarsByHorsePower([
+                cars.createCar ('red', 10),
+                cars.createCar ('blue', 2),
+                cars.createCar ('yellow', 30)
+            ]);
+
+            expect(sortCarsPower).toEqual([
+                { color: 'blue', power: 2 },
+                { color: 'red', power: 10 },
+                { color: 'yellow', power: 30 }, 
+            ]);     
         });
 
         it('should sort the array if it is sorted the wrong way around', () => {
+            const sortCarsPower = cars.sortCarsByHorsePower([
+                cars.createCar ('yellow', 30),
+                cars.createCar ('red', 10),
+                cars.createCar ('blue', 2)
+            ]);
+
+            expect(sortCarsPower).toEqual([
+                { color: 'blue', power: 2 },
+                { color: 'red', power: 10 },
+                { color: 'yellow', power: 30 }, 
+            ]);     
         });
 
         it('should do nothing if the array is empty', () => {
+            const sortCarsPower = cars.sortCarsByHorsePower([]);
+
+            expect(sortCarsPower).toEqual([]);
         });
     });
 
     describe('findCarWithSmallestPower', () => {
         it('should find the car with smallest power if it is at the first position of the array', () => {
+            const findSmallestPower = cars.findCarWithSmallestPower([
+                cars.createCar ('yellow', 2),
+                cars.createCar ('red', 10),
+                cars.createCar ('blue', 30)
+            ]);
+
+            expect(findSmallestPower).toEqual({
+                  color: 'yellow', 
+                  power: 2
+                });
         });
 
         it('should find the car with smallest power if it is at the last position of the array', () => {
+            const findSmallestPower = cars.findCarWithSmallestPower([
+                cars.createCar ('yellow', 30),
+                cars.createCar ('red', 10),
+                cars.createCar ('blue', 2)
+            ]);
+
+            expect(findSmallestPower).toEqual({
+                color: 'blue', 
+                power: 2
+            });
         });
 
         it('should find the car with smallest power', () => {
+            const findSmallestPower = cars.findCarWithSmallestPower([
+                cars.createCar ('yellow', 30),
+                cars.createCar ('red', 10),
+                cars.createCar ('blue', 2)
+            ]);
+
+            expect(findSmallestPower).toEqual({
+                color: 'blue',
+                power: 2
+            });
         });
 
         it('should find the first car with smallest power if two cars have the same power', () => {
+            const findSmallestPower = cars.findCarWithSmallestPower([
+                cars.createCar ('yellow', 30),
+                cars.createCar ('red', 10),
+                cars.createCar ('violet', 2),
+                cars.createCar ('blue', 2)
+            ]);
+
+            expect(findSmallestPower).toEqual({
+                color: 'violet', 
+                power : 2
+            });
         });
 
         it('should return undefined if the array is empty', () => {
+            const findSmallestPower = cars.findCarWithSmallestPower([]);
+
+            expect(findSmallestPower).toBeUndefined(); 
         });
     });
 
     describe('findCarWithBiggestPower', () => {
         it('should find the car with biggest power if it is at the first position of the array', () => {
+            const findBiggestPower = cars.findCarWithBiggestPower([
+                cars.createCar ('yellow', 80),
+                cars.createCar ('red', 10),
+                cars.createCar ('violet', 2),
+                cars.createCar ('blue', 30)
+            ]);
+
+            expect(findBiggestPower).toEqual({
+                color: 'yellow',
+                power: 80
+            });
         });
 
         it('should find the car with biggest power if it is at the last position of the array', () => {
+            const findBiggestPower = cars.findCarWithBiggestPower([
+                cars.createCar ('yellow', 20),
+                cars.createCar ('red', 10),
+                cars.createCar ('violet', 2),
+                cars.createCar ('red', 30)
+            ]);
+
+            expect(findBiggestPower).toEqual({
+                color: 'red',
+                power: 30
+            });
         });
 
         it('should find the car with biggest power', () => {
+            const findBiggestPower = cars.findCarWithBiggestPower([
+                cars.createCar ('yellow', 80),
+                cars.createCar ('red', 10),
+                cars.createCar ('violet', 2),
+                cars.createCar ('blue', 30)
+            ]);
+
+            expect(findBiggestPower).toEqual({
+                color: 'yellow',
+                power: 80,
+            }); 
         });
 
         it('should find the first car with biggest power if two cars have the same power', () => {
+            const findBiggestPower = cars.findCarWithBiggestPower([
+                cars.createCar ('yellow', 80),
+                cars.createCar ('red', 30),
+                cars.createCar ('violet', 30),
+                cars.createCar ('blue', 4)
+            ]);
+
+            expect(findBiggestPower).toEqual({
+                color: 'yellow',
+                power: 80
+            });
         });
 
         it('should return undefined if the array is empty', () => {
+            const findBiggestPower = cars.findCarWithBiggestPower([]);
+
+            expect(findBiggestPower).toBeUndefined(); 
         });
     });
 
     describe('countCarsByColors', () => {
         it('should count the cars by color if there is only one color', () => {
+            const countCarsColor = cars.countCarsByColors([
+                cars.createCar ('marine', 6),
+                cars.createCar ('marine', 30),
+                cars.createCar ('marine', 30),
+                cars.createCar ('marine', 4)
+            ]);
+
+            expect(countCarsColor).toEqual(
+                { marine: 4 }    
+            );
         });
 
         it('should count the cars by color while being case sensitive', () => {
+            const countCarsColor = cars.countCarsByColors([
+                cars.createCar ('Marine', 6),
+                cars.createCar ('marine', 30),
+                cars.createCar ('marine', 30),
+                cars.createCar ('marine', 4)
+            ]);
+
+            expect(countCarsColor).toEqual({
+                Marine: 1,
+                marine: 3   
         });
+    });
 
         it('should count the cars', () => {
+            const countCarsColor = cars.countCarsByColors([
+                cars.createCar ('yellow', 6),
+                cars.createCar ('red', 30),
+                cars.createCar ('violet', 30),
+                cars.createCar ('blue', 4),
+                cars.createCar ('blue', 8),
+                cars.createCar ('blue', 50)
+            ]);
+
+            expect(countCarsColor).toEqual({
+                yellow: 1,
+                red: 1,
+                violet: 1,
+                blue: 3
+            });
         });
 
         it('should return an empty count if the array is empty', () => {
+            const countCarsColor = cars.countCarsByColors([]); 
+
+            expect(countCarsColor).toEqual({});
         });
     });
 
     describe('countCarsByColorsCaseInsensitiveCaseInsensitive', () => {
         it('should count the cars by color if there is only one color', () => {
+            const countCarsColorCaseInsensitive = cars.countCarsByColorsCaseInsensitive([
+                cars.createCar ('blue', 6),
+                cars.createCar ('blue', 30),
+                cars.createCar ('blue', 30),
+                cars.createCar ('Blue', 4),
+                cars.createCar ('blue', 8),
+                cars.createCar ('blue', 50)
+            ]);
+
+            expect(countCarsColorCaseInsensitive).toEqual({
+                blue: 6,
+            })
         });
 
         it('should count the cars by color while being case insensitive', () => {
+            const countCarsColorCaseInsensitive = cars.countCarsByColorsCaseInsensitive([
+                cars.createCar ('purple'),
+                cars.createCar ('Purple'),
+                cars.createCar ('Purple'),
+                cars.createCar ('Purple'),
+                cars.createCar ('Purple')
+            ]);
+
+            expect(countCarsColorCaseInsensitive).toEqual({
+                purple: 5
+            });
         });
 
         it('should count the cars', () => {
+            const countCarsColorCaseInsensitive = cars.countCarsByColorsCaseInsensitive([
+                cars.createCar ('purple'),
+                cars.createCar ('purple'),
+                cars.createCar ('blue'),
+                cars.createCar ('blue'),
+                cars.createCar ('blue'),
+                cars.createCar ('red'),
+                cars.createCar ('black'),
+                cars.createCar ('black'),
+                cars.createCar ('brown')
+            ]);
+
+            expect(countCarsColorCaseInsensitive).toEqual({
+                purple: 2,
+                blue: 3,
+                red: 1,
+                black: 2,
+                brown: 1
+            });
         });
 
         it('should return an empty count if the array is empty', () => {
+            const countCarsColorCaseInsensitive = cars.countCarsByColorsCaseInsensitive([]);
+
+            expect(countCarsColorCaseInsensitive).toEqual({});
         });
     });
 });
